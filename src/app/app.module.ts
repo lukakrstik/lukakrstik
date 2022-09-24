3eddd389f7f56cb03db197d3150993943c16f560
Luka Krstikj <luka.krstik@protonmail.com>
2022-09-24 15:41:57 +0200

    reseno edno, otvoreno drugo cetvrti deo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
