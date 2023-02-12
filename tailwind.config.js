a09461aff7619fe9ac99aadad3a82e99d5b15ef8
Luka Krstikj <luka.krstik@protonmail.com>
2023-02-12 19:36:13 +0100

    Redesign initial push 3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        krstic: '#171717',
+        backaccent: '#cdffe8'
+      }
+    },
+  },
+  plugins: [],
+};
