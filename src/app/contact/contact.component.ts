090cefa5dec441e716095a09b5bd7478eeb5527d
Luka Krstikj <luka.krstik@protonmail.com>
2023-12-25 14:15:36 +0100

    Dynamic Age change

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css', '../../../node_modules/bootstrap/dist/css/bootstrap.css']
 })
 export class ContactComponent implements OnInit {
-
+  age:number = 0;
+  date:Date = new Date()
   constructor() { }
-
   ngOnInit(): void {
     document.body.classList.add('gradRGB');
+    if (this.date.getMonth() >= 10) this.age = this.date.getFullYear() - 2002;
+    else this.age = this.date.getFullYear()-2003;
   }
   ngOnDestroy(): void {
     document.body.classList.remove('gradRGB');
